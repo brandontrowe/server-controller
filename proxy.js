628ed2a679237e17ac5fa4d58f982f3e9534204a
brandonrowe <brandon.rowe@gmail.com>
2016-11-20 14:29:38 -0500

    inital commit

--- proxy.js
+++ proxy.js
@@ -2,19 +2,9 @@
 
 const httpProxy = require('http-proxy');
 const http = require('http');
-const https = require('https');
 const fs = require('fs');
 
-const secureOptions = {
-    key: fs.readFileSync('notif.brandontrowe.com.key', 'utf8'),
-    cert: fs.readFileSync('notif.brandontrowe.com.crt', 'utf8'),
-    passphrase: 'password'
-}
 const proxy = httpProxy.createProxy();
-const secureProxy = httpProxy.createProxy({
-    ssl: secureOptions,
-    secure: true
-})
 
 // Get registered subdomains and routes
 let routes = {};
@@ -41,23 +31,6 @@ http.createServer((req, res) => {
 }).listen(80, () => {
     console.log('http proxy started')
 });
-
-https.createServer(secureOptions, (req, res) => {
-        if (routes && routes[req.headers.host]) {
-            secureProxy.web(req, res, {
-                target: routes[req.headers.host]
-            });
-        } else {
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            for (let property in routes) {
-                res.write('<div><a href="http://' + property + '">' + property +'</a></div>');
-            }
-            res.end();
-        }
-    }).listen(443, () => {
-        console.log('https proxy started')
-    }
-);
 
 proxy.on('upgrade', (req, socket, head) => {
     proxy.ws(req, socket, head);
